20629811e503dd5acbc5f0552ddc90921aa475a4
kimdae993 <kimdae@hanyang.ac.kr>
2026-05-20 18:37:40 +0900

    [ifx] PaymentStore

--- server/routes/RestAPI/PaymentStore.js
+++ server/routes/RestAPI/PaymentStore.js
@@ -76,7 +76,7 @@ async function sendToPNT(paymentResponse, inferenceResult, folderPath, paymentAt
         const rfidTime =
         String(now.getFullYear()).slice(2) +
         String(now.getMonth() + 1).padStart(2, "0") +
-        String(now.getDate()).padStart(2, "0") +
+        String(now.getDate()).padStart(2, "0");
         // String(now.getHours()).padStart(2, "0") +
         // String(now.getMinutes()).padStart(2, "0") +
         // String(now.getSeconds()).padStart(2, "0");
