4d257727ddee4bdc266e40c4444f39640e968bad
kimdae993 <kimdae@hanyang.ac.kr>
2026-05-28 15:06:30 +0900

    [fix]

--- server/config/dev.js
+++ server/config/dev.js
@@ -17,18 +17,18 @@ module.exports = {
     // 사원증(RFID) 매장 브로커ID
 
     // 매장 고유번호
-    divisionIdx: 'DI17647205538493077',
+    // divisionIdx: 'DI17647205538493077',
     // 테스트용
     // divisionIdx: 'DI17790813642907755', // 학습용
     // divisionIdx: 'DI17647205538493077',
-    // divisionIdx: 'DI17798460900133031', // 통테
+    divisionIdx: 'DI17798460900133031', // 통테
     
     // 장비 고유번호
     // deviceIdx: 'DE17560868094789999',
-    deviceIdx: 'DE17683631997086480',
+    // deviceIdx: 'DE17683631997086480',
     // 테스트용
     // deviceIdx: 'DE17790815108130388', // 학습용
-    // deviceIdx: 'DE17798461293792881', // 통테
+    deviceIdx: 'DE17798461293792881', // 통테
 
     // divisionIdx: 'DI17790813642907755',
     // deviceIdx: 'DE17790815108130388',
