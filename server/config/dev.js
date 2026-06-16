726bf34d118f8160e8dcbcf8fddaae4d12ea5472
yoona-J <chungyn@hanyang.ac.kr>
2026-06-16 15:49:41 +0900

    change Idx into frozen device version

--- server/config/dev.js
+++ server/config/dev.js
@@ -16,22 +16,22 @@ module.exports = {
 
     // 사원증(RFID) 매장 브로커ID
 
-    // 매장 고유번호
+    // 냉장 매장 고유번호
     // divisionIdx: 'DI17647205538493077',
     // 테스트용
-    divisionIdx: 'DI17790813642907755', // 학습용
-    // divisionIdx: 'DI17647205538493077',
-    // divisionIdx: 'DI17798460900133031', // 통테
+    // divisionIdx: 'DI17790813642907755', // 냉장 학습용 (여의도점)
+    // divisionIdx: 'DI17798460900133031', // 냉장 통테용 (부산점)
+    divisionIdx: 'DI17790813642907755', // 냉동 테스트용
     
-    // 장비 고유번호
+    
+    // 냉장 장비 고유번호
     // deviceIdx: 'DE17560868094789999',
     // deviceIdx: 'DE17683631997086480',
     // 테스트용
-    deviceIdx: 'DE17790815108130388', // 학습용
-    // deviceIdx: 'DE17798461293792881', // 통테
-
-    // divisionIdx: 'DI17790813642907755',
-    // deviceIdx: 'DE17790815108130388',
+    // deviceIdx: 'DE17790815108130388', // 냉장 학습용
+    // deviceIdx: 'DE17798461293792881', // 냉장 통테용
+    deviceIdx: 'DE17815818605453073', // 냉동 테스트용 
+    
     // PNT RestAPI 연결
     restApi: 'https://apichaidev.atcrk.co.kr/api/v1',
     userId: 'chaitest',   
