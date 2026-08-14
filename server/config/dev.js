5f50815215beac38829c54c0bac646d668dd322b
yoona-J <chungyn@hanyang.ac.kr>
2026-08-14 11:58:06 +0900

    change idx

--- server/config/dev.js
+++ server/config/dev.js
@@ -28,7 +28,8 @@ module.exports = {
     // divisionIdx: 'DI17790813642907755', // 냉장 학습용 (여의도점)
     // divisionIdx: 'DI17798460900133031', // 냉장 통테용 (부산점)
     // divisionIdx: 'DI17790813642907755', // 냉동 테스트용
-    divisionIdx: 'DI17846216186054023',
+    // divisionIdx: 'DI17846216186054023', // 냉장 테스트용 (한양대점)
+    divisionIdx: 'DI17866730866133136', // 냉장 테스트용 (ITBT점)
     
     
     // 냉장 장비 고유번호
@@ -38,7 +39,8 @@ module.exports = {
     // deviceIdx: 'DE17790815108130388', // 냉장 학습용
     // deviceIdx: 'DE17798461293792881', // 냉장 통테용
     // deviceIdx: 'DE17815818605453073', // 냉동 테스트용 
-    deviceIdx: 'DE17846226165932307',
+    // deviceIdx: 'DE17846226165932307',  // 냉장 테스트용 (한양대점)
+    deviceIdx: 'DE17866760140506643',  // 냉장 테스트용 (ITBT점)
     
     // PNT RestAPI 연결
     restApi: 'https://apichaidev.atcrk.co.kr/api/v1',
